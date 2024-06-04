d0035a1dd19636ef4800933c4f7b3aeab99e0354
core-ryan <ryanjr0620@yahoo.com>
2024-06-03 23:21:21 -0600

    - Added finishedTasks property on Project class to track finished finishedTasks - Added method of Project to remove tasks from class' "tasks" property

--- src/project-item.js
+++ src/project-item.js
@@ -1,7 +1,11 @@
 export default function createProjectItem (newName) {
     const name = newName;
     let tasks = [];
+    let finishedTasks = [];
     const addTask = (newTask) => tasks.push(newTask);
+    const removeTask = (index) => {
+        tasks.splice(index, 1);
+    };
 
-    return {name, tasks, addTask};
+    return {name, tasks, finishedTasks, addTask, removeTask};
 }
